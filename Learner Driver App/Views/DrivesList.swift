190ee341660c9873d137a3582bce61ab9ab9a1d2
Alex Fogg <alex.fogg@icloud.com>
2022-01-28 12:32:51 +1100

    Changed the entire front page to a scrollview

--- Learner Driver App/Views/DrivesList.swift
+++ Learner Driver App/Views/DrivesList.swift
@@ -10,78 +10,69 @@ import SwiftUI
 struct DrivesList: View {
     var body: some View {
         NavigationView {
-            VStack (spacing: 0) {
-                
-                ZStack {
-                    Color.gray
-                        .opacity(0.1)
-                        .edgesIgnoringSafeArea(.all)
-                        .frame(height: 300)
-                
-                    HStack {
-                        Spacer()
-                        DriveProgressBars(
-                            totalProgress: calculateTotalProgres(drives: drives),
-                            dayProgress: calculateDayProgres(drives: drives),
-                            nightProgress: calculateNightProgres(drives: drives)
-                        )
-                            .frame(width: 200.0, height: 200.0)
-                            .padding(0.0)
-                        
-                        VStack {
+            ScrollView {
+                VStack (spacing: 0) {
+                    
+                    ZStack {
+                        Color.gray
+                            .opacity(0.1)
+                            .edgesIgnoringSafeArea(.all)
+                            .frame(height: 300)
+                    
+                        HStack {
                             Spacer()
-                                .padding(.vertical, 30.0)
+                            DriveProgressBars(
+                                totalProgress: calculateTotalProgres(drives: drives),
+                                dayProgress: calculateDayProgres(drives: drives),
+                                nightProgress: calculateNightProgres(drives: drives)
+                            )
+                                .frame(width: 200.0, height: 200.0)
+                                .padding(0.0)
                             
-                            AddADriveButton()
-                                .padding(.horizontal)
-                                .padding(.top, 30)
-                        }
+                            VStack {
+                                Spacer()
+                                    .padding(.vertical, 30.0)
+                                
+                                AddADriveButton()
+                                    .padding(.horizontal)
+                                    .padding(.top, 30)
+                            }
 
-                    }
-                }
-                .navigationBarTitle("")
-                .navigationBarHidden(true)
-                .navigationBarBackButtonHidden(true)
-                
-                Divider()
-                
-                ZStack {
-                    Color.gray
-                        .opacity(0.1)
-                        .frame(height: 50)
-                    HStack {
-                        Text("Your Drives")
-                            .font(.title)
-                            .multilineTextAlignment(.leading)
-                            .padding()
-                        Spacer()
-                    }
-                }
-                
-//                List(drives) { drive in
-//                    NavigationLink {
-//                        DriveDetailsView(drive: drive)
-//                    } label: {
-//                        DrivesRow(drive: drive)
-//                    }
-//
-//                }
-                
-                ForEach(drives, id: \.self) { drive in
-                    VStack {
-                        NavigationLink(destination: DriveDetailsView(drive: drive)) {
-                            DrivesRow(drive: drive)
                         }
-                        
                     }
-
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                    .navigationBarBackButtonHidden(true)
+                    
                     Divider()
+                    
+                    ZStack {
+                        Color.gray
+                            .opacity(0.1)
+                            .frame(height: 50)
+                        HStack {
+                            Text("Your Drives")
+                                .font(.title)
+                                .multilineTextAlignment(.leading)
+                                .padding()
+                            Spacer()
+                        }
+                    }
+                    
+                        ForEach(drives, id: \.self) { drive in
+                            NavigationLink {
+                               DriveDetailsView(drive: drive)
+                           } label: {
+                               DrivesRow(drive: drive)
+                           }
+                            Divider()
+
+                        }
+
+                        Divider()
+                    
+    //                NavigationBar()
                 }
-                
-                .border(Color.yellow, width: 3)
-                .background(Color.blue)
-                
-                NavigationBar()
             }
         }
     }
