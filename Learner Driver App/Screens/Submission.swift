39af53a0aee32acc7b64909a7f2898d1c3302490
Alex Fogg <alex.fogg@icloud.com>
2022-06-22 16:54:19 +1000

    Finished submission page

--- Learner Driver App/Screens/Submission.swift
+++ Learner Driver App/Screens/Submission.swift
@@ -8,19 +8,121 @@
 import SwiftUI
 
 struct Submission: View {
+    @EnvironmentObject var personalContainer: PersonalContainer
+    @EnvironmentObject var drivesContainer: DrivesContainer
+    
     var body: some View {
         Form {
             Section(header: Text("Requirements")) {
-                Text("Total hours:")
-                Text("Night hours:")
-                Text("Hazards:")
+                
+                HStack {
+                    Text("Total hours:")
+                    Spacer()
+                    Text("\(calculateTotalHours(drives: drivesContainer.drives)) / 120")
+                }
+                
+                HStack {
+                    Text("Night hours:")
+                    Spacer()
+                    Text("\(calculateNightHours(drives: drivesContainer.drives)) / 20")
+                }
+                
+                HStack {
+                    Text("Hazards:")
+                    Spacer()
+                    if personalContainer.personal.hpt == true {
+                        Text("Completed")
+                            .multilineTextAlignment(.trailing)
+                    } else {
+                        Text("Not Completed")
+                    }
+                }
+                
             }
             
+            
             Section(header: Text("Optionals")) {
-                Text("Safer Drivers:")
+                HStack {
+                    Text("Safer Drivers:")
+                    Spacer()
+                    if personalContainer.personal.sdc == true {
+                        Text("Completed")
+                            .multilineTextAlignment(.trailing)
+                    } else {
+                        Text("Not Completed")
+                    }
+                }
+            }
+            
+            Section(footer:
+                        HStack(alignment: .center) {
+                Spacer()
+                
+                if personalContainer.personal.hpt == true && calculateTotalHours(drives: drivesContainer.drives) == 120 {
+                    Text("Completed")
+                        .fontWeight(.semibold)
+                        .font(.title3)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .cornerRadius(30)
+                } else {
+                    Text("Not yet completed")
+                        .fontWeight(.semibold)
+                        .font(.title3)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(30)
+                }
+                
+                Spacer()
+                
+            }) {
+                EmptyView()
             }
         }
         .navigationTitle("Submission")
+    }
+    
+    public func calculateTotalHours(drives: [Drive]) -> Int {
+        var hoursDriven = 0.0
+        for drive in drives {
+            hoursDriven += Double(drive.endOdometer) - Double(drive.startOdometer)
+        }
+        
+        var result = Int(hoursDriven)
+        if result >= 120 {
+            result = 120
+        }
+        
+        if result <= 0 {
+            result = 0
+        }
+        
+        
+        return result;
+    }
+    
+    func calculateNightHours(drives: [Drive]) -> Int {
+        var hoursDriven = 0.0
+        for drive in drives {
+            if drive.isDayTime == false {
+                hoursDriven += Double(drive.endOdometer) - Double(drive.startOdometer)
+            }
+        }
+        
+        var result = Int(hoursDriven)
+        
+        if result >= 20 {
+            result = 20
+        }
+        
+        if result <= 0 {
+            result = 0
+        }
+        
+        return result;
     }
 }
 
